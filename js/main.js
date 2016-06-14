7269fa3518eb7f49da7eb7071562daae873f1057
Aleksandr Filimonenko <filimonenko.aleksandr@gmail.com>
2016-06-14 23:23:49 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
-// var myNum = Number(123);
-var myNum = 123;
-console.log(myNum);
+/*поменять перменные местами
+чтобы a было равно 20
+а b было равно 10
+(с третьей перменной или без)*/
 
-var str1 = "Привет и 'пока'";
-var str2 = 'Пока и "привет"';
-console.log(str1);
-console.log(str2);
+var a = 10;
+var b = 20;
+
+// first method
+var c = a;
+a = b;
+b = c;
+
+// second method
+a += b;
+b = a - b;
+a = a - b;
+
+//http://www.quizful.net/interview/problems/swap-without-temp-var
+b = [a, a = b][0]
+
+console.log(a);
+console.log(b);
+
+
+//////////////////////////////////////////////////////////////////
+/*Для трех переменных, с помощью конструкции if -
+найти самое большее число и умножить его на 1000,
+затем следующее по величине число умножить на 100
+С оставшимся третьим числом ничего не делать.*/
+var c = 3;
+var d = 2;
+var e = 1;
+
+
+if (c > d && c > e) {
+	c *= 1000;
+	d > e ? d *= 100 : e *= 100
+
+} else if (e > d && e > c) {
+	e *= 1000
+	d > c ? d *= 100 : с *= 100;
+
+} else if (d > c && d > e) {
+	d *= 1000;
+	c > e ? c *= 100: e *= 100;
+}
+
+console.log(c);
+console.log(d);
+console.log(e);
+
+
+
